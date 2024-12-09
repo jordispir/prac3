379e520a1342eccd6485cc6dfd6b6f227b37c817
Greenmarq <diego.marquez@estudiants.urv.cat>
2024-12-09 21:04:03 +0100

    Alumnos y classe membres

---
+++ Alumnes.java
@@ -0,0 +1,52 @@
+public class Alumnes extends membre {
+    private String ensenyament; 
+    private int anysETSE;
+    private boolean esGraduat;
+
+    // Constructor
+    public Alumnes(String alies, String emailInstitucional, String dataAlta, String ensenyament, int anysETSE, boolean esGraduat) {
+        super(alies, emailInstitucional, dataAlta);
+        this.ensenyament = ensenyament;
+        this.anysETSE = anysETSE;
+        this.esGraduat = esGraduat;
+    }
+
+    // Getters i Setters
+    public String getEnsenyament() {
+        return ensenyament;
+    }
+
+    public void setEnsenyament(String ensenyament) {
+        this.ensenyament = ensenyament;
+    }
+
+    public int getAnysETSE() {
+        return anysETSE;
+    }
+
+    public void setAnysETSE(int anysETSE) {
+        this.anysETSE = anysETSE;
+    }
+
+    public boolean isEsGraduat() {
+        return esGraduat;
+    }
+
+    public void setEsGraduat(boolean esGraduat) {
+        this.esGraduat = esGraduat;
+    }
+
+    // Sobreescriptura del mètode toString()
+    @Override
+    public String toString() {
+        return "Alumne{" +
+                "alies='" + getAlies() + '\'' +
+                ", emailInstitucional='" + getEmailInstitucional() + '\'' +
+                ", dataAlta=" + getDataAlta() +
+                ", dataBaixa=" + getDataBaixa() +
+                ", ensenyament='" + ensenyament + '\'' +
+                ", anysETSE=" + anysETSE +
+                ", esGraduat=" + esGraduat +
+                '}';
+    }
+}
